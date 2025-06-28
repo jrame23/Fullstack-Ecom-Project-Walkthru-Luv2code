d4aa157b07b4c3d829266aa7e9b0033f7b43449c
jrame23 <jacobcramey1999@gmail.com>
2025-06-28 19:46:45 -0400

    Changes to project files

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,20 +1,32 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { ProductList } from './components/product-list/product-list';
+import { provideHttpClient } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing-module';
 
+import { Routes, RouterModule } from '@angular/router';
+import { ProductCategoryMenu } from './components/product-category-menu/product-category-menu';
+const routes: Routes = [
+  {path: 'category/:id', component: ProductList},
+  {path: 'category', component: ProductList},
+  {path: 'products', component: ProductList},
+  {path: '', redirectTo: '/products', pathMatch: 'full' },
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+];
 @NgModule({
   declarations: [
-    App
+    App,
+    ProductList,
+    ProductCategoryMenu
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [
-    provideBrowserGlobalErrorListeners()
-  ],
+  providers: [provideHttpClient()],
   bootstrap: [App]
 })
 export class AppModule { }
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductList } from './components/product-list/product-list';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductList }  // default route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
